a795750d8dd649ca14f31195e2aa19af6c6ea81c
zxzhaoxuan <504242598@qq.com>
2019-01-09 15:45:35 +0800

    aaa

--- src/test/java/Service1.java
+++ src/test/java/Service1.java
@@ -2,12 +2,13 @@ import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.HashMap;
-import java.util.Map;
+
 
 /**
  * @program: ssm
@@ -18,18 +19,38 @@ import java.util.Map;
 public class Service1 {
     @Test
     public void doService1() throws Exception {
-        FileInputStream fileInputStream=new FileInputStream("src/nio.txt");
-        FileChannel fileChannel=fileInputStream.getChannel();
-        ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
-        fileChannel.read(byteBuffer);
-        byteBuffer.flip();
-        while (byteBuffer.hasRemaining()){
-            byte b=byteBuffer.get();
-            System.out.print((char) b);
+        try(RandomAccessFile ra=new RandomAccessFile("src/nio.txt","rw"); FileChannel fc=ra.getChannel();
+        ){
+            ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
+            String data="New String to write to file..." + System.currentTimeMillis();
+            byteBuffer.put(data.getBytes());
+            byteBuffer.flip();
+            fc.write(byteBuffer);
+
+            while (fc.read(byteBuffer)!=-1) {
+                byteBuffer.flip();
+                while (byteBuffer.hasRemaining()){
+                    System.out.print((char) byteBuffer.get());
+                }
+                byteBuffer.clear();
+            }
         }
     }
     @Test
     public void doService2() {
-        System.out.println(3<<1);
+        try(FileInputStream fileInputStream=new FileInputStream("src/nio.txt"); FileWriter fileWriter=new FileWriter("src/qqq.txt")){
+            InputStreamReader inputStreamReader=new InputStreamReader(fileInputStream);
+            char[] chars=new char[2];
+            while (inputStreamReader.read(chars)!=-1){
+                for (char c:chars){
+                    fileWriter.write(c);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
