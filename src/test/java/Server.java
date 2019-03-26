ad9b29c22d5bef113d7d9688a4b115425d35bd68
zhaoxuan <504242598@qq.com>
2019-03-26 15:53:20 +0800

    aaa

--- src/test/java/Server.java
+++ src/test/java/Server.java
@@ -14,28 +14,28 @@ import java.util.Iterator;
  * @create: 2019-01-09 15:14
  **/
 public class Server {
-    public static void main(String[] args){
-        try(ServerSocketChannel serverSocketChannel=ServerSocketChannel.open()){
+    public static void main(String[] args) {
+        try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
             System.out.println("服务器已启动...");
             serverSocketChannel.socket().bind(new InetSocketAddress(9999));
             serverSocketChannel.configureBlocking(false);
-            Selector selector=Selector.open();
+            Selector selector = Selector.open();
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-            while (selector.select()>0){
-                Iterator<SelectionKey> iterator=selector.selectedKeys().iterator();
-                while (iterator.hasNext()){
-                    SelectionKey key=iterator.next();
-                    if (key.isAcceptable()){
-                        SocketChannel socketChannel=serverSocketChannel.accept();
+            while (selector.select() > 0) {
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey key = iterator.next();
+                    if (key.isAcceptable()) {
+                        SocketChannel socketChannel = serverSocketChannel.accept();
                         socketChannel.configureBlocking(false);
-                        socketChannel.register(selector,SelectionKey.OP_READ);
-                    }else if (key.isReadable()){
-                        SocketChannel socketChannel=(SocketChannel) key.channel();
-                        ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
-                        int len=0;
-                        while ((len=socketChannel.read(byteBuffer))!=-1){
+                        socketChannel.register(selector, SelectionKey.OP_READ);
+                    } else if (key.isReadable()) {
+                        SocketChannel socketChannel = (SocketChannel) key.channel();
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                        int len = 0;
+                        while ((len = socketChannel.read(byteBuffer)) != -1) {
                             byteBuffer.flip();
-                            System.out.println(new String(byteBuffer.array(),0,len));
+                            System.out.println(new String(byteBuffer.array(), 0, len));
                             byteBuffer.clear();
                         }
                     }
--- src/test/java/Service1.java
+++ src/test/java/Service1.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
+import org.springframework.util.DigestUtils;
 
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -8,6 +10,8 @@ import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -18,31 +22,39 @@ import java.nio.channels.FileChannel;
  **/
 public class Service1 {
     @Test
+    public void md5(){
+        String base="123456";
+
+        String md5= DigestUtils.md5DigestAsHex(base.getBytes());
+        System.out.println(md5);
+    }
+    @Test
     public void doService1() throws Exception {
-        try(RandomAccessFile ra=new RandomAccessFile("src/nio.txt","rw"); FileChannel fc=ra.getChannel();
-        ){
-            ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
-            String data="New String to write to file..." + System.currentTimeMillis();
+        try (RandomAccessFile ra = new RandomAccessFile("src/nio.txt", "rw"); FileChannel fc = ra.getChannel()) {
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            String data = "New String to write to file..." + System.currentTimeMillis();
             byteBuffer.put(data.getBytes());
             byteBuffer.flip();
             fc.write(byteBuffer);
 
-            while (fc.read(byteBuffer)!=-1) {
+            while (fc.read(byteBuffer) != -1) {
                 byteBuffer.flip();
-                while (byteBuffer.hasRemaining()){
+                while (byteBuffer.hasRemaining()) {
                     System.out.print((char) byteBuffer.get());
                 }
                 byteBuffer.clear();
             }
         }
     }
+
     @Test
     public void doService2() {
-        try(FileInputStream fileInputStream=new FileInputStream("src/nio.txt"); FileWriter fileWriter=new FileWriter("src/qqq.txt")){
-            InputStreamReader inputStreamReader=new InputStreamReader(fileInputStream);
-            char[] chars=new char[2];
-            while (inputStreamReader.read(chars)!=-1){
-                for (char c:chars){
+        try (FileInputStream fileInputStream = new FileInputStream("src/nio.txt"); FileWriter fileWriter = new FileWriter("src/qqq.txt")) {
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedInputStream bufferedInputStream=new BufferedInputStream(fileInputStream);
+            char[] chars = new char[2];
+            while (inputStreamReader.read(chars) != -1) {
+                for (char c : chars) {
                     fileWriter.write(c);
                 }
             }
@@ -52,5 +64,12 @@ public class Service1 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    @Test
+    public void test2(){
+        List<Integer> a= Arrays.asList(1,2,3,4,5);
+        a.stream().filter(n->n>3).peek(n->n=n+2).forEach(System.out::println);
+        //Stream.generate(()->new Random().nextInt(100)).limit(20).forEach(System.out::println);
+
     }
 }
